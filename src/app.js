2a15ffb9ee37f462d20abf058481993292d3288f
hiepxuan <hiepxuan2006@gmail.com>
2023-03-28 18:28:02 +0700

    tét1

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
 const express = require("express")
-const app = express()
+const path = require("path")
+const bodyParser = require("body-parser")
+const passport = require("passport")
+const session = require("express-session")
+var cors = require("cors")
 const port = 3000
+app.use(cors())
+// body-parser
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(require("./app.routes"))
 app.get("/", (req, res) => {
