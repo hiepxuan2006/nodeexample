074a091d8373c7e9d48cf610f9f3f6427f2b8583
hiepxuan <hiepxuan2006@gmail.com>
2023-03-28 18:33:31 +0700

    tét1

--- src/app.routes.js
+++ src/app.routes.js
@@ -5,6 +5,5 @@ router.get("/ping", (req, res) => {
   res.send("pingpong")
 })
 
-const sliderCtrl = require("./controller/sliderController")
 // router.get("/slider/get-sliders", sliderCtrl.getSlider)
 module.exports = router
