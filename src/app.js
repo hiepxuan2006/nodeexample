a5e42a717db65039b64b0d043d3211861c961dd0
hiepxuan <hiepxuan2006@gmail.com>
2023-03-30 11:55:45 +0700

    1

--- src/app.js
+++ src/app.js
@@ -2,12 +2,6 @@ const express = require("express")
 const app = express()
 const port = 3000
 
-setTimeout(async () => {
-  await require("./connection/db").connectDB()
-  await require("./connection/redisConnection").connectRedis()
-  // app.use("/api/", require("./app.routes"))
-}, 0)
-
 app.use(require("./app.routes"))
 app.get("/", (req, res) => {
   res.send("Hello World!")
